244c1a9608dbaf79645349752f17e6d2cc782b3e
h0lybyte <admin@kbve.com>
2014-05-06 23:30:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -138,12 +138,17 @@ socket.on('disconnect', function() {
 		secure: true
 	});
 });
-
+/**
 function contains(string, terms) {
 	for (var i = 0; i < terms.length; i++) {
 		if (string.toLowerCase().indexOf(terms[i].toLowerCase()) == -1) {
 			return false;
 		}
 	}
+	return true;
+}
+**/
+function contains(string, terms){
+	if(string.indexOf(terms) === -1){ return false; }
 	return true;
 }
